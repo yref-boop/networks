5728e1083f57aeb817d240d6a1ef7f82ecce0183
yref-boop <regofdez.yago@gmail.com>
2023-02-06 23:06:57 +0100

    feat: implemented file information reader

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ this repository stores the 2 practicalss implemented on networks labs
 the first practical aims to some basic concepts, specifically, we are asked to implement the following:
 - implement an application on `es.udc.redes.tutorial.copy.Copy` for copying a file (with either text or binary content) in the local disk using data streams
 the application will recieve two input parameters:
-```
-java es.udc.redes.tutorial.copy.Copy <source file> <destination file>
-```
+    `java es.udc.redes.tutorial.copy.Copy <source file> <destination file>`
+- implement an application for obtaining the main properties of a file:
+    - size
+    - last modification date
+    - name
+    - extension
+    - file type
+    - absolute path
+    the application will recieve one input parameter:
+    `java es.udc.redes.tutorial.info.Info <source file>`
 - udp echo server
 - tcp echo server
 ### p1 - web server
--- src/es/udc/redes/tutorial/info/Info.java
+++ src/es/udc/redes/tutorial/info/Info.java
@@ -1,9 +1,44 @@
 package es.udc.redes.tutorial.info;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.io.IOException;
+
 public class Info {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        try {
+
+            Path path = Paths.get(args[0]);
+            BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+
+            System.out.println("size: " + attributes.size());
+            System.out.println("last modified time: " + attributes.lastModifiedTime());
+            String filename = path.getFileName().toString();
+            if (filename.contains(".")) {
+                String[] tokens = filename.split("\\.");
+                System.out.println("name: " + tokens[0]);
+                System.out.println("extension: " + tokens[1]);
+            }
+            else {
+                System.out.println("name: " + filename);
+                System.out.println("extension not applicable");
+            }
+            if (attributes.isRegularFile())
+                System.out.println("filetype: " + Files.probeContentType(path));
+            if (attributes.isDirectory())
+                System.out.println("filetype: directory");
+            if (attributes.isSymbolicLink())
+                System.out.println("filetype: symbolic-link");
+            if (attributes.isOther())
+                System.out.println("filetype: other");
+            System.out.println("absolute path: " + path.toAbsolutePath().toString());
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
-    
 }
