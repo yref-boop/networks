daf7ef93d6a961873584b6e231fa43ad96c58d83
yref-boop <regofdez.yago@gmail.com>
2023-03-15 11:17:28 +0100

    feat: implemented main thread behaviour

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -2,29 +2,84 @@ package es.udc.redes.webserver;
 
 import java.net.*;
 import java.io.*;
-
+import java.util.*;
+import java.text.*;
 
 public class ServerThread extends Thread {
 
+    BufferedReader input;
+    DataOutputStream output;
+    PrintWriter printer;
+    File file;
+    Date date;
+    long modified;
+
     private Socket socket;
 
-    public ServerThread(Socket s) {
-        // Store the socket s
-        this.socket = s;
-    }
+    public ServerThread(Socket socket) {this.socket = socket;}
 
     public void run() {
         try {
-          // This code processes HTTP requests and generates 
-          // HTTP responses
-          // Uncomment next catch clause after implementing the logic
-          //
-        // } catch (SocketTimeoutException e) {
-        //    System.err.println("Nothing received in 300 secs");
+            input  = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+            output = new DataOutputStream (socket.getOutputStream());
+            printer = new PrintWriter (socket.getOutputStream(), true);
+
+            String line = input.readLine();
+            StringTokenizer token = new StringTokenizer (line);
+            String method = token.nextToken();
+            String file_name   = token.nextToken();
+            file_name = ("p1-files" + file_name);
+
+            FileInputStream input_stream = null;
+            boolean found = true;
+
+            try {
+                input_stream = new FileInputStream (file_name);
+            } catch (FileNotFoundException e) {
+                found = false;
+            }
+
+            date = new Date ();
+            file = new File (file_name);
+            modified = file.lastModified();
+            SimpleDateFormat date_format = new SimpleDateFormat ("MMMM dd, yyyy, hh:mm a");
+            String server = "/nServer: WebServer";
+
+            String STATUS_LINE, HEADER_LINE;
+            String BODY = null;
+            if (!found) {
+                STATUS_LINE = "HTTP/1.0 404 NOT FOUND";
+                HEADER_LINE = "Date: " + date;
+                BODY =  "<html>\n\t<head>\n\t\t<title>\n\t\t\tERROR 404\n\t\t</title>\n\t</head>\n\t<body>\n\t\t<p>ERROR 404</p>\n\t</boddy>\n</html>";
+            } else {
+                STATUS_LINE = "HTTP/1.0 200 OK";
+                HEADER_LINE = "Date: " + date + server + "\nContentLength: " + file.length() + "\nContentType: TODO" + "\nLast-Modified: " + date_format.format(modified);
+                HEADER_LINE = "Date: " + date;
+            }
+            printer.println (STATUS_LINE);
+            printer.println (HEADER_LINE);
+            if ((!found) && (BODY != null))
+                printer.println (BODY);
+            else {
+                printer.println ("");
+                int n;
+                if ((method != "HEAD") && (input_stream != null)){
+                    n = input_stream.read ();
+                    while (n != -1)
+                        output.write(n);
+                }
+                input_stream.close ();
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         } finally {
-            // Close the client socket
+            try{ socket.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
