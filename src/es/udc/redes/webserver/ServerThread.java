1a23e9f1397b4ebc45c7374ddd961b0cb7ac0e7a
yref-boop <regofdez.yago@gmail.com>
2023-03-17 01:56:54 +0100

    feat: implemented server thread

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -7,91 +7,181 @@ import java.text.*;
 
 public class ServerThread extends Thread {
 
-    BufferedReader input;
-    DataOutputStream output;
-    PrintWriter printer;
-    File file;
-    Date date;
-    long modified;
-
     private Socket socket;
 
     public ServerThread(Socket socket) {this.socket = socket;}
 
     public void run() {
         try {
-            input  = new BufferedReader (new InputStreamReader (socket.getInputStream()));
-            output = new DataOutputStream (socket.getOutputStream());
-            printer = new PrintWriter (socket.getOutputStream(), true);
+            BufferedReader reader  = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+            String message = reader.readLine();
+            System.out.println ("SERVER: Received " + message + " from "
+                                + socket.getInetAddress ().toString () + ":"
+                                + socket.getPort ());
 
-            String line = input.readLine();
-            StringTokenizer token = new StringTokenizer (line);
-            String method = token.nextToken();
-            String file_name   = token.nextToken();
-            file_name = ("p1-files" + file_name);
+            if (message == null) return;
 
-            FileInputStream input_stream = null;
-            boolean found = true;
+            String[] message_parts = message.split (" ");
+            File file = new File ("p1-files" + message_parts[1]);
 
-            try {
-                input_stream = new FileInputStream (file_name);
-            } catch (FileNotFoundException e) {
-                found = false;
-            }
+            if (!file.exists()) {
 
-            date = new Date ();
-            file = new File (file_name);
-            modified = file.lastModified();
-            SimpleDateFormat date_format = new SimpleDateFormat ("MMMM dd, yyyy, hh:mm a");
-            String server = "/nServer: WebServer";
+                file = new File ("p1-files/error404.html");
+                FileInputStream input = new FileInputStream (file.toString());
+                OutputStream client_output = socket.getOutputStream();
+                client_output.write (("HTTP/1.0 404 Not Found\r\n").getBytes());
 
-            String STATUS_LINE, HEADER_LINE;
-            String BODY = null;
-            if (!found) {
-                STATUS_LINE = "HTTP/1.0 404 NOT FOUND";
-                HEADER_LINE = "Date: " + date;
-                BODY =  "<html>\n\t<head>\n\t\t<title>\n\t\t\tERROR 404\n\t\t</title>\n\t</head>\n\t<body>\n\t\t<p>ERROR 404</p>\n\t</boddy>\n</html>";
-            } else {
-                STATUS_LINE = "HTTP/1.0 200 OK";
-                HEADER_LINE = "Date: " + date + server + "\nContentLength: " + file.length() + "\nContentType: " + contentType(file_name) + "\nLast-Modified: " + date_format.format(modified);
-                HEADER_LINE = "Date: " + date;
-            }
-            printer.println (STATUS_LINE);
-            printer.println (HEADER_LINE);
-            if ((!found) && (BODY != null))
-                printer.println (BODY);
-            else {
-                printer.println ("");
-                int n;
-                if ((method != "HEAD") && (input_stream != null)){
-                    n = input_stream.read ();
-                    while (n != -1)
-                        output.write(n);
+                DateFormat date_format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+                String date = date_format.format(new Date());
+                String modification_date = date_format.format(file.lastModified());
+
+                client_output.write (("Date: " + date + "\r\n").getBytes());
+                client_output.write (("Server: web_server\r\n").getBytes());
+                client_output.write (("Content-Length: " + file.length()+"\r\n").getBytes());
+                content_type (file, client_output);
+                client_output.write (("Last-Modified:" + modification_date +"\r\n").getBytes());
+                client_output.write (("\r\n").getBytes());
+
+                if (!message_parts[0].equals ("HEAD")) {
+                    int c;
+                    while ((c = input.read()) != -1)
+                        client_output.write(c);
                 }
-                input_stream.close ();
+                client_output.flush();
+                client_output.close();
+                return;
             }
+
+            if (message_parts[0].equals ("GET")) {
+
+                FileInputStream input = new FileInputStream (file.toString());
+                OutputStream client_output = socket.getOutputStream();
+                boolean isModifiedSince = true;
+                Date modifiedSince;
+                DateFormat sdf = new SimpleDateFormat ("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+                String line = reader.readLine();
+                while(!line.equals("")) {
+                    String[] line_parts = line.split(": ");
+                    if (line_parts[0].equals ("If-Modified-Since")){
+                        Date fechaUltimaMod = new Date (file.lastModified());
+                        modifiedSince = sdf.parse (line_parts[1]);
+                        if (!fechaUltimaMod.before (modifiedSince)) {
+                            isModifiedSince = false;
+                            break;
+                        }
+                    }
+                    line = reader.readLine();
+                }
+                if (!isModifiedSince)
+                    client_output.write (("HTTP/1.0 304 Not Modified\r\n").getBytes());
+                else
+                    client_output.write (("HTTP/1.0 200 OK\r\n").getBytes());
+
+                DateFormat date_format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+                String date = date_format.format(new Date());
+                String modification_date = date_format.format(file.lastModified());
+
+                client_output.write (("Date: " + date + "\r\n").getBytes());
+                client_output.write (("Server: web_server\r\n").getBytes());
+                client_output.write (("Content-Length: " + file.length()+"\r\n").getBytes());
+                content_type (file, client_output);
+                client_output.write (("Last-Modified:" + modification_date +"\r\n").getBytes());
+                client_output.write (("\r\n").getBytes());
+
+                if (isModifiedSince) {
+                    int c;
+                    while ((c = input.read()) != -1)
+                        client_output.write(c);
+                }
+                    client_output.flush();
+                    client_output.close();
+                return;
+            }
+
+            if (message_parts[0].equals ("HEAD")) {
+
+                OutputStream client_output = socket.getOutputStream();
+                boolean isModifiedSince = true;Date modifiedSince;
+                DateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+                String line = reader.readLine();
+                while(!line.equals("")) {
+                    String[] line_parts = line.split(": ");
+                    if (line_parts[0].equals("If-Modified-Since")){
+                        Date fechaUltimaMod = new Date(file.lastModified());
+                        modifiedSince = sdf.parse(line_parts[1]);
+                        if (!fechaUltimaMod.before(modifiedSince)) {
+                            isModifiedSince = false;
+                            break;
+                        }
+                    }
+                    line = reader.readLine();
+                }
+                if (!isModifiedSince)
+                    client_output.write(("HTTP/1.0 304 Not Modified\r\n").getBytes());
+                else
+                    client_output.write(("HTTP/1.0 200 OK\r\n").getBytes());
+
+                DateFormat date_format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+                String date = date_format.format(new Date());
+                String modification_date = date_format.format(file.lastModified());
+
+                client_output.write (("Date: " + date + "\r\n").getBytes());
+                client_output.write (("Server: web_server\r\n").getBytes());
+                client_output.write (("Content-Length: " + file.length()+"\r\n").getBytes());
+                content_type (file, client_output);
+                client_output.write (("Last-Modified:" + modification_date +"\r\n").getBytes());
+                client_output.write (("\r\n").getBytes());
+
+                client_output.flush();
+                client_output.close();
+                return;
+            }
+
+            file = new File ("p1-files/error404.html");
+            FileInputStream input_stream = new FileInputStream (file.toString());
+            OutputStream client_output = socket.getOutputStream();
+
+            DateFormat date_format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",Locale.ENGLISH);
+            String date = date_format.format(new Date());
+            String modification_date = date_format.format(file.lastModified());
+
+            client_output.write(("HTTP/1.0 400 Bad Request\r\n").getBytes());
+            client_output.write (("Date: " + date+ "\r\n").getBytes());
+            client_output.write (("Server: web_server\r\n").getBytes());
+            client_output.write (("Content-Length: " + file.length()+"\r\n").getBytes());
+            content_type (file, client_output);
+            client_output.write (("Last-Modified:" + modification_date +"\r\n").getBytes());
+            client_output.write (("\r\n").getBytes());
+
+            int c;
+            while ((c = input_stream.read()) != -1)
+                client_output.write(c);
+            client_output.flush();
+            client_output.close();
+
         } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
-        } finally {
-            try{ socket.close();
-            }
-            catch (IOException e){
+        }
+        finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-    private String contentType (String file) {
-        if (file.endsWith (".html"))
-            return ("text/html");
-        else if (file.endsWith (".txt"))
-            return ("text/plain");
-        else if (file.endsWith (".gif"))
-            return ("image/gif");
-        else if (file.endsWith (".png"))
-            return ("image/png");
-        else
-            return ("application/octet-stream");
+
+    public void content_type(File file,OutputStream output) throws IOException {
+        String[] parts = file.toString().split("\\.");
+        switch (parts[1]) {
+            case "html" -> output.write(("Content-Type: text/html\r\n").getBytes());
+            case "txt" -> output.write(("Content-Type: text/plain\r\n").getBytes());
+            case "gif" -> output.write(("Content-Type: image/gif\r\n").getBytes());
+            case "png" -> output.write(("Content-Type: image/png\r\n").getBytes());
+            default -> output.write(("Content-Type: application/octet-stream\r\n").getBytes());
+        }
     }
 }
+
